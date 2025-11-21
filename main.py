9e0d0d3a13816742d2f441e28986b8a7a0891c48
JulioCaue <juliocaue22@gmail.com>
2025-11-20 21:50:44 -0300

    Versão com codico de pegar clima transformado em uma função.

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
-import sys
-import requests
+#define função de pegar clima
+def get_clima(cidade):
 
-local=input('digite o cidade que quer ver a temperatura ou digite sair para sair: ')
+    import requests
+    # 1. Montar URL de Geocoding usando 'cidade'
+    Localizador = (f"https://geocoding-api.open-meteo.com/v1/search?name={cidade}")
+    # 2. Fazer request e pegar lat/lon
+    Dados_Localização = requests.get(Localizador)
+    dados=Dados_Localização.json()
+    try:
+        lat=dados['results'][0]['latitude']
+        lon=dados['results'][0]['longitude']
+    # 3. Montar URL de Clima
+        Pegar_Temperatura = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
+    # 4. Fazer request e pegar temperatura
+        resposta = requests.get(Pegar_Temperatura)
+    # 5. return temperatura
+        if resposta.status_code == 200:
+            dados=resposta.json()
+            temperatura=dados['current_weather']['temperature']
+            return temperatura
+            
+        else:
+            return None
+    except:
+        return None
+#Logica de input/output
+while True:
+    cidade=input('Digite o nome da ciadade ou digite sair: ')
+    if cidade==('sair'):
+        break
 
-if str(local)==('sair'):
-    sys.exit()
-
-Localizador = (f"https://geocoding-api.open-meteo.com/v1/search?name={local}")
-
-Dados_Localização = requests.get(Localizador)
-
-dados=Dados_Localização.json()
-
-try:
-    lat=dados['results'][0]['latitude']
-    lon=dados['results'][0]['longitude']
-
-    # 1. Definir para onde vamos ligar (URL)
-    Pegar_Temperatura = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
-
-    # 2. Fazer a chamada (GET)
-    resposta = requests.get(Pegar_Temperatura)
-
-    # 3. Ver se funcionou (200 = OK)
-    if resposta.status_code == 200:
-        print("Conexão bem sucedida!")
-        print("Dados recebidos:")
-        dados=resposta.json()
-        temperatura=dados['current_weather']['temperature']
-        print (f'A temperatura atual em {local.capitalize()} é {temperatura}°C')
-        
+    temperatura=get_clima(cidade)
+    #Se não for none, deu certo
+    if temperatura is not None:
+        print (f'A temperatura atual em {cidade.capitalize()} é {temperatura}°C')
+    #se for none, deu ruim.
     else:
-        print("Deu erro na conexão:", resposta.status_code)
-except:
-    print ('local não existe.')
+        print("Cidade não encontrada. Tente de novo.")
